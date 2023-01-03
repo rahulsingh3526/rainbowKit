054f8d6e231f9d6f9fed03a214588c73b779277e
Rahul_Singh <koderrahul@gmail.com>
2023-01-04 00:06:27 +0530

    ok

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,37 @@
-import '../styles/globals.css'
+import dynamic from "next/dynamic";
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { mainnet, polygon, polygonMumbai,goerli } from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const { chains, provider } = configureChains(
+  [mainnet, polygon, polygonMumbai, goerli],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  chains
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+})
+
+ function App({ Component, pageProps }) {
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+       <Component {...pageProps} />
+     </RainbowKitProvider>
+    </WagmiConfig>
+  )
 }
+
+export default dynamic (() => Promise.resolve(App), {ssr: false})
